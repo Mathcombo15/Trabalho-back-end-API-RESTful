e44d49789f8632990dee31f3a8e3fad4b9e7d2ec
Leticia Melo <leticiamelodia@gmail.com>
2025-11-23 12:54:14 -0300

    integração com o banco

--- app.js
+++ app.js
@@ -1,18 +1,67 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2');
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'usuario_app',
+  password: 'senha_forte_123',
+  database: 'db_animals'
+})
+
+connection.connect()
+
+app.get("/animals", (req, res) => {
+     connection.query('select * from animals', function(err, rows, fields) {
+        if (err) throw err
+        res.status(200).json({ 
+            message: rows,
+            status: 'success1'
+        });
+    })
+});
+
+app.get("/animals/:id", (req, res) => {
+    res.status(200).json({ 
+        message: 'Buscar um animal',
+        status: 'success'
+    });
+});
+
+app.post("/animals", (req, res) => {
+    let name = 'onca pintada'
+    connection.query('insert into animals(name) values(?)',[name], function(err) {
+        if (err) throw err
+        res.status(200).json({ 
+            message: 'cadastrado com sucesso',
+            status: 'success1'
+        });
+    })
+    
+});
+
+app.put("/animals/:id", (req, res) => {
+    res.status(200).json({ 
+        message: 'Editar animal',
+        status: 'success'
+    });
+});
+
+app.delete("/animals/:id", (req, res) => {
+    res.status(200).json({ 
+        message: 'Deletar animal',
+        status: 'success'
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor escutando na porta ${PORT}`);
 });
-
-app.get("/", (req, res) => {
-    res.send('API de Backend está funcionando!');
-});
-
 
 module.exports = app;
